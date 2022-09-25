4686878b9ca3b99ea12fe7ada8bff1df051e6d9b
littlebutt <luogan199686@gmail.com>
2022-09-25 08:32:45 +0800

    chore: setup.py and more support

--- setup.py
+++ setup.py
@@ -1,11 +1,20 @@
 from setuptools import setup
 
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+
 setup(
     name='fubuki-iot',
     version='0.1',
     python_requires=">=3.8",
     packages=['iot', 'iot.core', 'iot.core.hardware', 'iot.core.acoustics', 'iot.core.semantics', 'iot.integration'],
+    install_requires=['keyboard>=0.13.5', 'loguru>=0.6.0', 'paho-mqtt>=1.6.1', 'PyAudio>=0.2.12', 'pydantic>=1.10.2',
+                      'python-dotenv>=0.21.0', 'requests>=2.28.1'],
     url='https://github.com/littlebutt/fubuki-iot',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     license='MIT License',
     author='littlebutt',
     author_email='luogan199686@gmail.com',
